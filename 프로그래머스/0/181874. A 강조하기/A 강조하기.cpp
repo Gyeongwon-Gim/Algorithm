dfe5f09f0a8d42c5324f560c0e9c548eefae0ae3
Gyeongwon-Gim <92427216+Gyeongwon-Gim@users.noreply.github.com>
2026-05-08 01:54:29 +0900

    [level 0] Title: A 강조하기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string myString) {
+    for(int i = 0; i < myString.size(); i++){
+        if(myString[i] == 'a' || myString[i] == 'A'){
+            myString[i] = 'A';
+        } else {
+            myString[i] = tolower(myString[i]);
+        }
+    }
+    return myString;
+}
