0a63b479da6976f627d2b9103b0ac53962ba61c8
Gyeongwon-Gim <92427216+Gyeongwon-Gim@users.noreply.github.com>
2026-09-01 14:11:49 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.01 ms, Memory: 4.95 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string) {
+    string answer = "";
+    for (int i=my_string.length(); i > 0; i--) {
+        answer += my_string[i-1];
+    }
+    return answer;
+}
